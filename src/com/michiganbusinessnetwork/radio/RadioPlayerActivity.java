be2ca4ccd13c3f55ea5d078ffc5301405439c6c9
Dennis Cornwell <cornwe19@gmail.com>
2012-05-07 22:49:19 -0400

    Adding a simple streaming player to the radio player activity.

--- src/com/michiganbusinessnetwork/radio/RadioPlayerActivity.java
+++ src/com/michiganbusinessnetwork/radio/RadioPlayerActivity.java
@@ -1,9 +1,14 @@
 package com.michiganbusinessnetwork.radio;
 
+import java.io.IOException;
+
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
+import android.media.AudioManager;
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.util.Log;
 
 public class RadioPlayerActivity extends Activity {
 
@@ -12,11 +17,34 @@ public class RadioPlayerActivity extends Activity {
       context.startActivity( thisIntent );
    }
    
+   private MediaPlayer mPlayer;
+   
    @Override
    public void onCreate( Bundle savedInstanceState ) {
       super.onCreate( savedInstanceState );
       
       setContentView( R.layout.radio_player );
+      
+      mPlayer = new MediaPlayer();
+      try {
+         mPlayer.setDataSource( "http://radio.michiganbusinessnetwork.com:8000/;stream.nsv" );
+         mPlayer.prepare();
+         mPlayer.start();
+      }
+      catch ( IOException e ) {
+         Log.e( "RadioPlayer", "Failed to initialize media mPlayer" );
+         e.printStackTrace();
+      }
+      mPlayer.setAudioStreamType( AudioManager.STREAM_MUSIC );
    }
    
+   @Override
+   public void onStop() {
+      super.onStop();
+      
+      if ( mPlayer.isPlaying() ) {
+         mPlayer.stop();
+         mPlayer.release();
+      }
+   }
 }
