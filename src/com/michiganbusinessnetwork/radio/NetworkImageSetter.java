42e27b79d388146e43a65088002ec6efb827c27c
Dennis Cornwell <cornwe19@gmail.com>
2012-05-19 19:02:08 -0400

    Updating a lot of the UI

---
+++ src/com/michiganbusinessnetwork/radio/NetworkImageSetter.java
@@ -0,0 +1,17 @@
+package com.michiganbusinessnetwork.radio;
+
+public class NetworkImageSetter implements Advertisement.OnLoadedCallback {
+
+   @Override
+   public void onLoaded( Advertisement ad ) {
+      // TODO Auto-generated method stub
+      
+   }
+
+   @Override
+   public void onError( Exception e ) {
+      // TODO Auto-generated method stub
+      
+   }
+
+}
--- src/com/michiganbusinessnetwork/radio/RadioPlayerActivity.java
+++ src/com/michiganbusinessnetwork/radio/RadioPlayerActivity.java
@@ -1,10 +1,14 @@
 package com.michiganbusinessnetwork.radio;
 
 import java.io.IOException;
+import java.io.InputStream;
+import java.net.MalformedURLException;
 
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.media.AudioManager;
 import android.media.MediaPlayer;
 import android.media.MediaPlayer.OnPreparedListener;
@@ -13,8 +17,9 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.ImageButton;
+import android.widget.ImageView;
 
-public class RadioPlayerActivity extends Activity implements OnPreparedListener {
+public class RadioPlayerActivity extends Activity implements OnPreparedListener, Advertisement.OnLoadedCallback {
 
    public static void launch( Context context ) {
       Intent thisIntent = new Intent( context, RadioPlayerActivity.class );
@@ -26,6 +31,7 @@ public class RadioPlayerActivity extends Activity implements OnPreparedListener 
    
    private MediaPlayer mPlayer = null;
    private ImageButton mPlayPauseButton;
+   private Uri mAdvertisementUri;
    
    @Override
    public void onCreate( Bundle savedInstanceState ) {
@@ -34,9 +40,11 @@ public class RadioPlayerActivity extends Activity implements OnPreparedListener 
       setContentView( R.layout.radio_player );
       
       mPlayPauseButton = (ImageButton)findViewById( R.id.playPauseButton );
+      mPlayPauseButton.setEnabled( false );
       
       MediaPlayer player = new MediaPlayer();
       try {
+         player.setAudioStreamType( AudioManager.STREAM_MUSIC );
          player.setDataSource( MBN_STREAM_URL );
          player.setOnPreparedListener( this );
          player.prepareAsync();
@@ -45,17 +53,24 @@ public class RadioPlayerActivity extends Activity implements OnPreparedListener 
          Log.e( "RadioPlayer", "Failed to initialize media mPlayer" );
          e.printStackTrace();
       }
+      
+      Advertisement.loadAsync( Advertisement.MI_BUSINESS_AD_FEED, this );
    }
    
    public void onClickPlayPause( View v ) {
       if( mPlayer != null ) {
+         AudioManager manager = (AudioManager) getSystemService( Activity.AUDIO_SERVICE );
          if ( mPlayer.isPlaying() ) {
+            manager.abandonAudioFocus( null );
             mPlayer.pause();
-            mPlayPauseButton.setImageResource( android.R.drawable.ic_media_play );
+            mPlayPauseButton.setImageResource( R.drawable.play );
+            mPlayPauseButton.setBackgroundResource( R.drawable.play_button );
          }
          else {
+            manager.requestAudioFocus( null, AudioManager.STREAM_MUSIC, AudioManager.AUDIOFOCUS_GAIN );
             mPlayer.start();
-            mPlayPauseButton.setImageResource( android.R.drawable.ic_media_pause );
+            mPlayPauseButton.setImageResource( R.drawable.stop );
+            mPlayPauseButton.setBackgroundResource( R.drawable.stop_button );
          }
       }
    }
@@ -66,24 +81,79 @@ public class RadioPlayerActivity extends Activity implements OnPreparedListener 
    }
    
    @Override
-   public void onStop() {
-      super.onStop();
+   public void onPause() {
+      super.onPause();
       
       if ( mPlayer != null ) {
          if ( mPlayer.isPlaying() ) {
             mPlayer.stop();
          }
          
+         mPlayer.reset();
          mPlayer.release();
       }
    }
 
    @Override
    public void onPrepared( MediaPlayer player ) {
-      View loadingStreamProgress = findViewById( R.id.loadingAudioStreamProgress );
+      View loadingStreamProgress = findViewById( R.id.loadingStreamProgressBar );
       loadingStreamProgress.setVisibility( View.GONE );
+      mPlayPauseButton.setEnabled( true );
+      mPlayPauseButton.setImageResource( R.drawable.play );
       
-      player.setAudioStreamType( AudioManager.STREAM_MUSIC );
       mPlayer = player;
+   }
+
+   @Override
+   public void onLoaded( final Advertisement ad ) {
+      final ImageView adImage = (ImageView)findViewById( R.id.advertisement_image );
+      
+      Runnable downloadAndUpdateAdImage = new Runnable() {
+         @Override
+         public void run() {
+            final Bitmap adBitmap = downloadBitmapFromUrl( ad.mBannerImageUri );
+            
+            runOnUiThread( new Runnable() {
+               @Override
+               public void run() {
+                  if( adBitmap != null ) {
+                     adImage.setImageBitmap( adBitmap );
+                  }
+               }
+            });
+         }
+      };
+      
+      new Thread( downloadAndUpdateAdImage ).start();
+      
+      mAdvertisementUri = ad.mTargetUri;
+   }
+
+   public void onClickAdvertisement( View v ) {
+      Intent launchAdvertisement = new Intent( Intent.ACTION_VIEW, mAdvertisementUri );
+      startActivity( launchAdvertisement );
+   }
+   
+   private Bitmap downloadBitmapFromUrl( Uri url ) {
+      Bitmap imageBitmap = null;
+      
+      try {
+         InputStream imageStream = WebHelper.getResponseStream( url.toString() );
+         imageBitmap = BitmapFactory.decodeStream( imageStream );
+         imageStream.close();
+      }
+      catch ( MalformedURLException e ) {
+         // Don't bother user if the ad can't load for some reason
+      }
+      catch ( IOException e ) {
+         // Don't bother user if the ad can't load for some reason
+      }
+      
+      return imageBitmap;
+   }
+   
+   @Override
+   public void onError( Exception e ) {
+      
    }
 }
